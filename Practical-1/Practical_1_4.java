d71f1484c493f3189a6c66334e25791a4fbe8365
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-01-29 16:20:46 +0530

    Add files via upload

---
+++ Practical-1/Practical_1_4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Practical_1_4{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number(in metres) = ");
+		int meter = sc.nextInt();
+		
+		//Conversion of meter to feet
+		double feet = meter * 3.28084;
+		
+		System.out.println("Feet = " + feet);
+		
+		sc.close();
+	}
+}
