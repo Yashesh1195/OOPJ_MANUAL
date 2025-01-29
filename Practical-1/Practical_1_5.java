a03543a26479bece7919ae6ff7a0b4b318e57044
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-01-29 16:30:33 +0530

    Add files via upload

---
+++ Practical-1/Practical_1_5.java
@@ -0,0 +1,22 @@
+// 1 pound=.45359237 Kg and 1 inch=.0254 meters
+//import java.util.Scanner;
+//class Practical_1_5{
+//	public static void main(String args[]){
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("Enter the weight(in pounds) = ");
+//		int pound = sc.nextInt();
+//
+//		System.out.print("Enter the height(in inches) = ");
+//		int inch = sc.nextInt();
+//		
+//		//Conversion of meter to feet
+//		double meter = inch/0.0254;
+//		double kgs = pound/0.45359237;
+//
+//		double bmi = kgs/(meter * meter);
+//		
+//		System.out.println("BMI = " + bmi);
+//		
+//		sc.close();
+//	}
+//}
