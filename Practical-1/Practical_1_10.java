deebba89947ad6bf1e5250a86f043de50d0e66de
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-02-02 17:37:25 +0530

    Add files via upload

---
+++ Practical-1/Practical_1_10.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class Practical_1_10 {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter length of side a  : ");
+        double a = sc.nextDouble();
+        System.out.print("Enter length of side b  : ");
+        double b = sc.nextDouble();
+        System.out.print("Enter length of side c  : ");
+        double c = sc.nextDouble();
+
+
+        if( a + b > c && a + c > b && b + c > a)
+        {
+            double s = (double) (a+b+c)/2;
+
+            double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+            
+            System.out.printf("Area of triangle : %.2f\n", area);
+        }
+        else{
+            System.out.println("Given dimensions do not for a valid triangle!!");
+        }
+        
+        sc.close();
+
+    }
+}
