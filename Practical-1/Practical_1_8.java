067a949f3ef1e533471c57c0b062cc06d83a128e
Yashesh Mehta <155947815+Yashesh1195@users.noreply.github.com>
2025-02-02 16:53:53 +0530

    Add files via upload

--- Practical-1/Practical_1_8.java
+++ Practical-1/Practical_1_8.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
 public class Practical_1_8 {
-    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        //Available Denominations
+        int[] denomination = {100, 50, 10, 5, 2, 1};
+        
+        System.out.print("Enter the amount to be withdrawn:- ");
+        int amount = sc.nextInt();
+
+        //Storing the note count of each denomination
+        int[] noteCount = new int[denomination.length];
+
+        //Calculation of the number of notes of each denomination
+        for(int i=0; i<denomination.length; i++) {
+            if(amount >= denomination[i]) {
+                noteCount[i] = amount / denomination[i];
+                amount %= denomination[i];
+            }
+        }
+
+        System.out.println("Currency notes given:");
+
+        for(int i=0; i<denomination.length; i++) {
+            if(noteCount[i] > 0) {
+                System.out.println(denomination[i] + " : " + noteCount[i]);
+            }
+        }
+
+        sc.close();
+        
+    }
 }
